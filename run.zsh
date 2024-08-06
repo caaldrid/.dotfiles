0af3f04ec37c88ed2e7715af7e80eeaa5c339cb2
Carlos Aldridge <aldrichsea@outlook.com>
2024-08-06 14:44:57 -0700

    Added ability to unlink all stowed files by passing the main scrip the -c flag, stored p10k configuration, and renamed install to run

--- run.zsh
+++ run.zsh
@@ -1,5 +1,51 @@
 #!/usr/bin/env zsh
 
+# Default values
+clean_only=false
+
+# Function to handle long options
+parse_long_options() {
+  while [[ $# -gt 0 ]]; do
+    case $1 in
+      --clean-only)
+        clean_only=true
+        shift
+        ;;
+      --)
+        shift
+        break
+        ;;
+      -*)
+        echo "Unknown option: $1"
+        exit 1
+        ;;
+      *)
+        break
+        ;;
+    esac
+  done
+}
+
+# Parse single-dash options first
+while getopts "c" opt; do
+  case ${opt} in
+    c )
+      clean_only=true
+      ;;
+    \? )
+      echo "Usage: cmd [-c] [--clean-only]"
+      exit 1
+      ;;
+  esac
+done
+
+# Shift away the processed single-dash options
+shift $((OPTIND -1))
+
+# Now handle long options
+parse_long_options "$@"
+
+
 # Make sure the default shell is a zsh
 if [[ ! "$SHELL" ==  *"zsh"* ]]; then
 	echo >&2 "We expect zsh is set as the default shell"
@@ -29,15 +75,15 @@ else
 	exit 1
 fi
 
-echo "Will now stow the following directories: \n\t- $(echo $FOLDERS_TO_STOW | sed "s/,/\n\t- /g")"
+echo "Will now iterate through the following directories: \n\t- $(echo $FOLDERS_TO_STOW | sed "s/,/\n\t- /g")"
 
 # Loop through the given folders that we are told to stow and run command
 for folder in $(echo $FOLDERS_TO_STOW | sed "s/,/ /g")
 do
 	if [ -d "./$folder" ];then
-		echo "\n----------Stowing $folder----------"
+		echo "\n----------Processing $folder----------"
 	    	stow -v --target=$HOME -D $folder
-	    	stow -v --target=$HOME $folder
+	    	$clean_only || stow -v --target=$HOME $folder # don't run if we are in clean-only mode
 	else
 		echo >&2 "$folder does not exist in $pwd"
 		exit 1
--- shell/.config/zsh/.zshrc
+++ shell/.config/zsh/.zshrc
@@ -3,7 +3,7 @@ export ZSH="$HOME/.oh-my-zsh"
 
 # Set name of the theme to load 
 # See https://github.com/ohmyzsh/ohmyzsh/wiki/Themes
-ZSH_THEME="robbyrussell"
+ZSH_THEME="powerlevel10k/powerlevel10k"
 
 # Which plugins would you like to load?
 # Standard plugins can be found in $ZSH/plugins/
@@ -14,6 +14,9 @@ plugins=(git)
 
 # Oh-My-ZSH init
 source $ZSH/oh-my-zsh.sh
+
+# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
+[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
 
 # Initialize atuin
 ! which atuin >/dev/null 2>&1 || eval "$(atuin init zsh)"
