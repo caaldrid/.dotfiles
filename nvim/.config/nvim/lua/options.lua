7115a6ad54981d1a2a7ce7b43b255d233ab986a2
caaldrid <aldrichsea@outlook.com>
2024-08-23 12:26:47 -0700

    Move set for relativenumbers into the options.lua file as its supposed to be

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -1,20 +1,19 @@
 require "nvchad.options"
 
--- add yours here!
-
--- local o = vim.o
+local o = vim.o
 local g = vim.g
 
 g.clipboard = {
-  name = 'WslClipboard',
+  name = "WslClipboard",
   copy = {
-    ['+'] = 'clip.exe',
-    ['*'] = 'clip.exe',
+    ["+"] = "clip.exe",
+    ["*"] = "clip.exe",
   },
   paste = {
-    ['+'] = 'pwsh.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-    ['*'] = 'pwsh.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+    ["+"] = 'pwsh.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+    ["*"] = 'pwsh.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
   },
   cache_enabled = 0,
 }
 
+o.relativenumber = true
