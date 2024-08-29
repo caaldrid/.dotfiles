3726eed3da9696691a1bb1e8cdd51576a16733a8
caaldrid <aldrichsea@outlook.com>
2024-08-29 11:12:27 -0700

    Init Python DAP

--- nvim/.config/nvim/lua/configs/dap.lua
+++ nvim/.config/nvim/lua/configs/dap.lua
@@ -1,4 +1,30 @@
-local setup = function() end
+local setup = function()
+  local dap = require "dap"
+  local get_python_path = require "helpers.python-path"
+
+  dap.adapters.python = {
+    type = "executable",
+    command = get_python_path(vim.fn.getcwd()),
+    args = { "-m", "debugpy.adapter" },
+  }
+
+  dap.configurations.python = {
+    {
+      -- The first three options are required by nvim-dap
+      type = "python", -- the type here established the link to the adapter definition: `dap.adapters.python`
+      request = "launch",
+      name = "Launch file",
+
+      -- Options below are for debugpy, see https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings for supported options
+
+      program = "${file}", -- This configuration will launch the current file if used.
+      pythonPath = function()
+        -- debugpy supports launching an application with a different interpreter then the one used to launch debugpy itself.
+        return get_python_path(vim.fn.getcwd())
+      end,
+    },
+  }
+end
 
 ---@type NvPluginSpec
 local spec = {
