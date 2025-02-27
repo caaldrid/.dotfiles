a304915c7c57a992674a9e121cbf648217dc7f0e
caaldrid <aldrichsea@outlook.com>
2025-02-26 19:22:56 -0800

    Add vscode mapping file to separate setup.

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,4 +1,5 @@
 if vim.g.vscode then
+  require "configs.vscode_keymaps"
 else
   vim.g.base46_cache = vim.fn.stdpath "data" .. "/nvchad/base46/"
   vim.g.mapleader = " "
---
+++ nvim/.config/nvim/lua/configs/vscode_keymaps.lua
@@ -0,0 +1,23 @@
+local keymap = vim.keymap.set
+local opts = { noremap = true, silent = true }
+
+-- remap leader key
+keymap("n", "<Space>", "", opts)
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- yank to system clipboard
+keymap({"n", "v"}, "<leader>y", '"+y', opts)
+
+-- paste from system clipboard
+keymap({"n", "v"}, "<leader>p", '"+p', opts)
+
+-- better indent handling
+keymap("v", "<", "<gv", opts)
+keymap("v", ">", ">gv", opts)
+
+-- paste preserves primal yanked piece
+keymap("v", "p", '"_dP', opts)
+
+-- removes highlighting after escaping vim search
+keymap("n", "<Esc>", "<Esc>:noh<CR>", opts)
