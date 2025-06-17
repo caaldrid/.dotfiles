c22cc27d67f7eb96a6e2a1de1c8c1ed335c7676a
caaldrid <aldrichsea@outlook.com>
2025-06-17 14:56:05 -0700

    Enable Smart Gaps again and disable socket

--- wayland/.config/hypr/scripts/socket/hyprland-socket.sh
+++ wayland/.config/hypr/scripts/socket/hyprland-socket.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
-toggle_float_in_workspace() {
+smart_gaps() {
   CMDS=""
   address="0x$1"
   address=${address//'"'/''}
   workspace=$2
   class=$3
-  classes_ignor=("firefox" "alt.tab" "Calendar Reminders" "clipse.board" "about")
+
+  classes_ignor=("alt.tab" "Calendar Reminders" "clipse.board" "about")
   # If we are in the special magic workspace or the window is a steam app or simply firefox, then we ignore it
   if [[ "$workspace" == "special:magic" ]] || [[ "$class" == *"steam"* ]] || [[ $(echo "${classes_ignor[*]}" | grep -c "$class") -ne 0 ]]; then
     return 0
@@ -13,15 +14,15 @@ toggle_float_in_workspace() {
 
   window_count=$(hyprctl -j clients | jq '.[] | .workspace | .name ' | grep -c "$workspace")
   if [[ "$window_count" -eq 1 ]]; then
-    CMDS+="dispatch setfloating address:$address;"
-    CMDS+="dispatch resizewindowpixel exact 1488 837,address:$address;"
-    CMDS+="dispatch centerwindow;"
+    CMDS+="dispatch setprop address:$address noborder on;"
+    CMDS+="dispatch setprop address:$address norounding on;"
   else
-    all_open_windows=$(hyprctl -j clients | jq --arg WN "$workspace" '.[] | select (.workspace .name == $WN) | select (.floating) | .address')
+    all_open_windows=$(hyprctl -j clients | jq --arg WN "$workspace" '.[] | select (.workspace .name == $WN) | .address')
 
     for address in $all_open_windows; do
       address=${address//'"'/''}
-      CMDS+="dispatch togglefloating address:$address;"
+      CMDS+="dispatch setprop address:$address noborder off;"
+      CMDS+="dispatch setprop address:$address norounding off;"
     done
   fi
 
@@ -36,7 +37,7 @@ handle() {
   SOCKET_DATA=("${SOCKET_DATA[@]:1}")
   # read -r action workspace windowclass windowtitle <<<"$1"
   case $action in
-  openwindow) toggle_float_in_workspace "${SOCKET_DATA[@]}" ;;
+  openwindow) smart_gaps "${SOCKET_DATA[@]}" ;;
   esac
 }
 
