dbe6d6c1dbfad4311feac2ba9143d418e517db98
caaldrid <aldrichsea@outlook.com>
2025-06-25 14:10:13 -0700

    Use hyprland-socket to toggle laptop monitor when conntecting/disconnecting to external

--- wayland/.config/hypr/scripts/socket/hyprland-socket.sh
+++ wayland/.config/hypr/scripts/socket/hyprland-socket.sh
@@ -1,28 +1,20 @@
 #!/usr/bin/env bash
-smart_gaps() {
+monitor_added() {
+  # Lets move all the workspaces from the laptop screen to the attached monitor and then disable the laptop monitor
   CMDS=""
-  address="0x$1"
-  address=${address//'"'/''}
-  workspace=$2
-  class=$3
+  monitor_id=$1
+  monitor_desc=$3
+  workspaces_to_move=(1 2 3 4 5)
 
-  classes_ignor=("alt.tab" "Calendar Reminders" "clipse.board" "about")
-  # If we are in the special magic workspace or the window is a steam app or simply firefox, then we ignore it
-  if [[ "$workspace" == "special:magic" ]] || [[ "$class" == *"steam"* ]] || [[ $(echo "${classes_ignor[*]}" | grep -c "$class") -ne 0 ]]; then
-    return 0
-  fi
+  if [[ "$monitor_desc" == *"ASUSTek COMPUTER INC VG28UQL1A"* ]]; then
+    for workspace in "${workspaces_to_move[@]}"; do
+      CMDS+="dispatch moveworkspacetomonitor $workspace $monitor_id;"
+    done
 
-  window_count=$(hyprctl -j clients | jq '.[] | .workspace | .name ' | grep -c "$workspace")
-  if [[ "$window_count" -eq 1 ]]; then
-    CMDS+="dispatch setprop address:$address noborder on;"
-    CMDS+="dispatch setprop address:$address norounding on;"
-  else
-    all_open_windows=$(hyprctl -j clients | jq --arg WN "$workspace" '.[] | select (.workspace .name == $WN) | .address')
-
-    for address in $all_open_windows; do
-      address=${address//'"'/''}
-      CMDS+="dispatch setprop address:$address noborder off;"
-      CMDS+="dispatch setprop address:$address norounding off;"
+    other_monitors=$(hyprctl monitors -j | jq --argjson ID "$monitor_id" '.[] | select (.id != $ID) | .description')
+    for other_desc in "${other_monitors[@]}"; do
+      other_desc=${other_desc//'"'/''}
+      CMDS+="keyword monitor desc:$other_desc,disable;"
     done
   fi
 
@@ -31,14 +23,43 @@ smart_gaps() {
   fi
 }
 
+monitor_removed() {
+  # Re-enable laptop monitor
+  CMDS=""
+  monitor_desc=$3
+
+  if [[ "$monitor_desc" == *"ASUSTek COMPUTER INC VG28UQL1A"* ]]; then
+    CMDS+="keyword monitor desc:Chimei Innolux Corporation 0x1540,preferred,auto,auto;"
+  fi
+
+  if [[ -n $CMDS ]]; then
+    hyprctl -q --batch "$CMDS"
+  fi
+
+}
+
+load() {
+  monitor_info=$(hyprctl monitors -j | jq '.[] | "\(.id),\(.name),\(.description)"')
+
+  if [[ $(echo "$monitor_info" | wc -l) -eq 2 ]]; then
+    monitor_info=$(IFS=$'\n' && echo "${monitor_info[*]}" | grep -v 'Chimei Innolux')
+    readarray -d $',' -t PARAM <<<"${monitor_info//'"'/''}"
+    monitor_added "${PARAM[@]}"
+  fi
+
+}
+
 handle() {
   readarray -d $',' -t SOCKET_DATA <<<"$1"
   action="${SOCKET_DATA[0]}"
   SOCKET_DATA=("${SOCKET_DATA[@]:1}")
   # read -r action workspace windowclass windowtitle <<<"$1"
   case $action in
-  openwindow) smart_gaps "${SOCKET_DATA[@]}" ;;
+  monitoraddedv2) monitor_added "${SOCKET_DATA[@]}" ;;
+  monitorremovedv2) monitor_removed "${SOCKET_DATA[@]}" ;;
+  configreloaded) load ;;
   esac
 }
 
+load
 socat -U - UNIX-CONNECT:"$XDG_RUNTIME_DIR"/hypr/"$HYPRLAND_INSTANCE_SIGNATURE"/.socket2.sock | while read -r line; do handle "${line//'>>'/','}"; done
