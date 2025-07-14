0faaab9161cc57e6ce8d81f32bed5dbf882d5eea
caaldrid <aldrichsea@outlook.com>
2025-07-14 10:04:03 -0700

    get rid of alttab

--- wayland/.config/hypr/scripts/alttab/enable.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-hyprctl -q --batch "keyword animations:enabled false ; dispatch exec ghostty --background='black' --title=AltTab --class='alt.tab' --command='~/.config/hypr/scripts/alttab/alttab.sh $1' ; keyword unbind ALT, TAB ; keyword unbind ALT SHIFT, TAB ; dispatch submap alttab"
