20673e37661f63c8aac79e58e9a72bddc02c7217
caaldrid <aldrichsea@outlook.com>
2025-07-16 10:36:41 -0700

    re write logic branching for tmux-sessionnizer to properly handle when to make a new window and when to just switch the client to a new session

--- bin/.local/scripts/tmux-sessionizer.sh
+++ bin/.local/scripts/tmux-sessionizer.sh
@@ -6,7 +6,7 @@ load_brew
 if [[ $# -eq 1 ]]; then
   selected=$1
 else
-  selected=$(zoxide query --list | fzf --style full --color dark --preview "lsd -lag --blocks=git,name --color=always --icon=always --icon-theme=fancy --tree --depth=2 {}" --preview-window=:20%)
+  selected=$(zoxide query --list | fzf --style full --color dark --preview "lsd -lag --blocks=git,name --color=always --icon=always --icon-theme=fancy --tree --depth=2 {}" --preview-window=left:20%)
 fi
 
 if [[ -z $selected ]]; then
@@ -16,11 +16,18 @@ fi
 selected_name=$(basename "$selected" | tr . _)
 tmux_running=$(pgrep tmux)
 
-tmux_cmd="tmux attach-session -t $selected_name"
-if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then # If we aren't in a tmux session and if there is no running tmux process
-  tmux_cmd="tmux new-session -s $selected_name -c $selected"
-elif ! tmux has-session -t="$selected_name" 2>/dev/null; then # If there isn't already a tmux session with that name
-  tmux_cmd="tmux new-session -ds $selected_name -c $selected"
+# If there is no running tmux session then create a detached session and lunch a new window with the right code tag and attach to the session
+if [ -z "$tmux_running" ]; then
+  tmux new-session -ds "$selected_name" -c "$selected"
+  hyprctl dispatch exec "uwsm app -- ghostty --title='Code' --class='nvim.code'  --command='source $HOME/.local/scripts/load_brew.sh && load_brew && tmux attach-session -t $selected_name'"
+  exit 0
 fi
 
-hyprctl dispatch exec "uwsm app -- ghostty --title='Code' --class='nvim.code' --command='source $HOME/.local/scripts/load_brew.sh && load_brew && $tmux_cmd'"
+# If we are in a tmux session then check if we need to make a new session for the selection or just switch to it
+if [ -n "$TMUX" ]; then
+  if ! tmux has-session -t="$selected_name" 2>/dev/null; then
+    tmux new-session -ds "$selected_name" -c "$selected"
+  fi
+  tmux switch-client -t="$selected_name"
+  return 0
+fi
