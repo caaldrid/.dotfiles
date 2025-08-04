23c4aa67cec970b03150b96f8da2cc35476cae4c
Carlos Aldridge <35670497+caaldrid@users.noreply.github.com>
2025-08-04 10:41:56 -0700

    Delete wayland directory

--- wayland/.config/wofi/wofi-uwsm.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Prompt the user to select an app using wofi in drun mode
-SELECTED=$(wofi --show drun)
-
-# If a selection was made, launch it with uwsm
-if [[ -n "$SELECTED" ]]; then
-  uwsm app -- "$SELECTED" &
-fi
