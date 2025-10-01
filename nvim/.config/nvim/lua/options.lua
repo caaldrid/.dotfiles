71c4acbe26c6c9909737a1088969d7ecdf4b9c38
caaldrid <aldrichsea@outlook.com>
2025-10-01 15:15:32 -0700

    Auto create folds using nvim-treesitter

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -5,3 +5,8 @@ o.relativenumber = true
 
 -- Hide the end of buffer lines that were showing a tilda character before
 vim.wo.fillchars = "eob: "
+
+-- Auto create folds using nvim-treesitter
+vim.wo.foldmethod = "expr"
+vim.wo.foldexpr = "nvim_treesitter#foldexpr()"
+vim.wo.foldenable = false
