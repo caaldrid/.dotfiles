a44d139f38c3e81e63229e0a8c03a3bece33efab
caaldrid <aldrichsea@outlook.com>
2025-10-09 18:02:00 -0700

    Fixed treesitter after migration to new code in main branch, and also fixed code highliting and folding

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -82,6 +82,32 @@ else
       },
     },
   }
-
+  vim.api.nvim_create_autocmd("FileType", {
+    pattern = {
+      "vim",
+      "lua",
+      "vimdoc",
+      "c_sharp",
+      "go",
+      "gomod",
+      "bash",
+      "markdown",
+      "markdown_inline",
+      "python",
+      "javascript",
+      "typescript",
+      "dockerfile",
+      "gitignore",
+      "html",
+      "css",
+      "yaml",
+      "json",
+      "tsx",
+    },
+    callback = function()
+      vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+      vim.treesitter.start()
+    end,
+  })
   require "mappings"
 end
--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -8,5 +8,4 @@ vim.wo.fillchars = "eob: "
 
 -- Auto create folds using nvim-treesitter
 vim.wo.foldmethod = "expr"
-vim.wo.foldexpr = "nvim_treesitter#foldexpr()"
 vim.wo.foldenable = false
