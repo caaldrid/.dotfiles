f21ba8b9078f98288cd6c450b43304248992cff8
caaldrid <aldrichsea@outlook.com>
2026-04-13 11:19:24 -0700

    Add Obsidian CLI on mac to the path

--- shell/.config/custom/paths.zsh
+++ shell/.config/custom/paths.zsh
@@ -3,3 +3,8 @@ addToPath "$HOME/.cargo/bin"
 addToPath "$HOME/.local/scripts"
 addToPath "$HOME/.local/bin"
 addToPath "$HOME/.docker/bin/"
+
+os_name=$(uname -s)
+if [[ "$os_name" == "Darwin" ]]; then
+	addToPath "/Applications/Obsidian.app/Contents/MacOS"
+fi
